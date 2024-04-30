655ccc02017e0d2312aa15fbcf4ab9f52a1955df
choi-ik <ci980704@naver.com>
2024-04-30 10:30:01 +0900

    chore: TanStack query Provider 구성

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 100],
+    "subject-case": [0],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
     "type-case": [2, "always", "lower-case"],
---
+++ src/app/Provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+interface Props {
+  children: ReactNode;
+}
+
+function Provider({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+}
+
+export default Provider;
