e0995421928846040216303f3aa187ed8706ba61
choi-ik <ci980704@naver.com>
2024-05-22 19:16:06 +0900

    feat: 다크모드를 위한 NextThemesProvider 구현

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 interface Props {
-  children: ReactNode;
+  children: React.ReactNode;
 }
-
 export default function Provider({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <NextThemesProvider attribute="class" defaultTheme="system">
+        {children}
+      </NextThemesProvider>
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
