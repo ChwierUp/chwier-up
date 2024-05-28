57c7aeccea65582af84247edee30688cbb3b2934
choi-ik <ci980704@naver.com>
2024-05-28 10:47:17 +0900

    feat: 헤더 컴포넌트 구현

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Tip from "@/public/tip.svg";
+import Logo from "@/public/logo.svg";
+import ProfileIcon from "./ProfileIcon";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 z-10 h-16 w-full border-b border-accent-200 bg-bg-100 px-[113px]">
+      <div className="flex h-full w-full max-w-[1440px] items-center justify-between">
+        <figure className="flex shrink-0 items-center">
+          <Logo
+            className="cursor-pointer fill-black dark:fill-white"
+            aria-label="취얼업 로고"
+          />
+          <Tip
+            className="ml-10 cursor-pointer fill-black dark:fill-white"
+            aria-label="tip"
+          />
+        </figure>
+        <ProfileIcon className="rounded-full bg-bg-100 dark:bg-bg-100" />
+      </div>
+    </header>
+  );
+}
