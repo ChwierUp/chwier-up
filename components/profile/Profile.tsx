e76efcd756913b791312f929c0d5afa584995f8b
lunarmoon7 <zentechie7@gmail.com>
2024-05-29 14:48:46 +0900

    style: Todo의 프로필 컴포넌트 생성

---
+++ components/profile/Profile.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../avatar";
+
+interface ProfileProps {
+  imageUrl: string;
+  name?: string;
+}
+export default function Profile({ imageUrl, name }: ProfileProps) {
+  return (
+    <div className="flex items-center gap-5">
+      <Avatar className="h-10 w-10 border-2 border-black dark:border-white">
+        <AvatarImage src={imageUrl} alt="@shadcn" />
+        <AvatarFallback>프로필</AvatarFallback>
+      </Avatar>
+      <p className="text-lg font-semibold">{name}</p>
+    </div>
+  );
+}
