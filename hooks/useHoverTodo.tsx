2ac79678c73c7a17b6a2c88f06723322d4d3ccc6
lunarmoon7 <zentechie7@gmail.com>
2024-05-29 14:51:13 +0900

    feat: Todo와 관련된 커스텀 훅 생성
    - todo 컴포넌트의 hover 이벤트를 처리합니다.
    - todo 컴포넌트의 편집, 완료 이벤트를 처리합니다.

---
+++ hooks/useHoverTodo.tsx
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export const useHoverTodo = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [isHover, setIsHover] = useState(false);
+
+  const handleOnHoverIn = useCallback(() => {
+    setIsHover(true);
+  }, []);
+
+  const handleOnHoverOut = useCallback(() => {
+    setIsHover(false);
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    element.addEventListener("mouseenter", handleOnHoverIn);
+    element.addEventListener("mouseleave", handleOnHoverOut);
+
+    return () => {
+      element.removeEventListener("mouseenter", handleOnHoverIn);
+      element.removeEventListener("mouseleave", handleOnHoverOut);
+    };
+  }, [ref, handleOnHoverIn, handleOnHoverOut]);
+
+  return { ref, isHover };
+};
