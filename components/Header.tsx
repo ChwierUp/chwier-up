e7f975955f3d1bd9e8cb821b71065209fbfda8f1
choi-ik <ci980704@naver.com>
2024-07-06 01:50:46 +0900

    refactor: 기존의 Profile svg를 걷어내고 shadcn의 Avatar 컴포넌트 사용, 다른 페이지로 라우팅 할 수 있도록 NavigationMenu 컴포넌트 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,48 @@
+import Link from "next/link";
 import Logo from "@/public/logo.svg";
-import ProfileIcon from "./ProfileButton";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/NavigationMenu";
+
+const ROUTE_PAGE = [
+  {
+    path: "/todo",
+    pageName: "Todo",
+  },
+  {
+    path: "/article",
+    pageName: "Article",
+  },
+];
 
 export default function Header() {
   return (
     <header className="fixed top-0 z-10 h-16 w-full border-b border-accent-200 bg-bg-100 px-[113px]">
       <div className="flex h-full w-full max-w-[1440px] items-center justify-between">
-        <figure className="flex shrink-0 items-center">
+        <div className="flex shrink-0 items-center">
           <Logo
             className="cursor-pointer fill-black dark:fill-white"
             aria-label="취얼업 로고"
           />
-          <p className="ml-10 cursor-pointer fill-black dark:fill-white">Tip</p>
-        </figure>
-        <ProfileIcon className="rounded-full bg-bg-100 dark:bg-bg-100" />
+          <div className="ml-8 h-5 border" />
+          <NavigationMenu className="ml-3">
+            <NavigationMenuList>
+              {ROUTE_PAGE.map(({ path, pageName }) => (
+                <NavigationMenuItem key={pageName} className="pl-5">
+                  <Link href={path}>{pageName}</Link>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <Avatar className="h-8 w-8">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </div>
     </header>
   );
