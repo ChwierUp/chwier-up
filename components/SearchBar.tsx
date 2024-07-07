e4c72bbd9021a18e8f21a09adc8a4da7f4d9adf9
choi-ik <ci980704@naver.com>
2024-07-07 17:24:18 +0900

    feat: Search 아이콘과 input 컴포넌트를 활용하여 SearchBar 컴포넌트 구현

---
+++ components/SearchBar.tsx
@@ -0,0 +1,23 @@
+import { Search } from "lucide-react";
+import { Input } from "@/components/Input";
+import { cn } from "@/lib/utils";
+
+interface SearchBarProps {
+  onSubmit: () => void;
+  className: string;
+}
+
+export default function SearchBar({
+  onSubmit,
+  className,
+}: Partial<SearchBarProps>) {
+  return (
+    <form
+      className={cn("relative flex w-full items-center", className)}
+      onSubmit={onSubmit}
+    >
+      <Search color="#949494" className="absolute ml-2" aria-label="돋보기" />
+      <Input type="text" placeholder="키워드로 검색하세요." className="pl-10" />
+    </form>
+  );
+}
