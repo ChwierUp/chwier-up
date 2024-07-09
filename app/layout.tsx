8d7e2f52ebde282ce0a0f5a7c7fa663bf2e75f43
choi-ik <ci980704@naver.com>
2024-07-09 15:07:50 +0900

    chore: ThemeProvider 사용으로 인해 생긴 단순 경고를 제거하기 위해 suppressHydrationWarning 속성 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body className={inter.className}>
         <Provider>
           <Header />
