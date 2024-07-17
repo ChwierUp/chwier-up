8e4a0795e55dda326b782cb446632f5a9d5e70bf
SoJuSo <asdfg7123@gmail.com>
2024-07-18 00:47:11 +0900

    Revert "Merge branch 'feat/#23' of https://github.com/ChwierUp/chwier-up into feat/#27"
    This reverts commit 90c2822eecdbe79b12ee7b3bf3cff74bee0a6fbc, reversing
    changes made to 06eeb13659a0d26f1c3745f9fa93b344c3eec7ab.

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ReactNode } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { SessionProvider } from "next-auth/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -13,13 +12,11 @@ interface Props {
 
 export default function Provider({ children }: Props) {
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        <NextThemesProvider attribute="class" defaultTheme="system">
-          {children}
-        </NextThemesProvider>
-        <ReactQueryDevtools initialIsOpen={true} />
-      </QueryClientProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider attribute="class" defaultTheme="system">
+        {children}
+      </NextThemesProvider>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
