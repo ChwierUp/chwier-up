87013d54aa9bb723fa5d9b07a7ece55f20b4437d
lunarmoon7 <zentechie7@gmail.com>
2024-07-24 14:18:54 +0900

    fix: hydration mismatch 에러 수정

--- app/landing/_components/LandingLogo.tsx
+++ app/landing/_components/LandingLogo.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import LandingLight from "@/public/landing-light.png";
 import LandingDark from "@/public/landing-dark.png";
 
 export default function LandingLogo() {
+  const [mounted, setMounted] = useState(false);
   const { theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Image
