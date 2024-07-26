7a932ba31d8641a72c2af0e79631e7f9860531cb
choi-ik <ci980704@naver.com>
2024-07-26 12:00:59 +0900

    feat: Header 컴포넌트에서 auth 정보 가져와 프로필 사진 연동 기능 구현

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import { auth } from "@/auth";
 import Logo from "@/public/logo.svg";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
 
 import {
   NavigationMenu,
@@ -19,7 +20,9 @@ const ROUTE_PAGE = [
   },
 ];
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
   return (
     <header className="fixed top-0 z-10 h-16 w-full border-b border-accent-200 bg-bg-100 px-[113px]">
       <div className="flex h-full w-full max-w-[1440px] items-center justify-between">
@@ -40,7 +43,13 @@ export default function Header() {
           </NavigationMenu>
         </div>
         <Avatar className="h-8 w-8">
-          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarImage
+            src={
+              session?.user?.image
+                ? session.user.image
+                : "https://github.com/shadcn.png"
+            }
+          />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </div>
