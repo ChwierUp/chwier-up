69e8efc2d95f9de79913a1882c5b66607b92fc83
lunarmoon7 <zentechie7@gmail.com>
2024-07-30 11:48:24 +0900

    style: 새로운 색상 토큰 적용

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -25,10 +25,10 @@ export default function ArticleCard({
 }: ArticleCardProps) {
   return (
     <Card className="w-72 overflow-hidden rounded-2xl border-none">
-      <CardHeader className="bg-bg-100 p-0">
+      <CardHeader className="bg-mainbg p-0">
         <Skeleton className="aspect-video h-40 w-full rounded-2xl dark:bg-[#C4C4C4]" />
       </CardHeader>
-      <CardContent className="flex flex-col gap-4 bg-bg-100 p-0 py-5">
+      <CardContent className="flex flex-col gap-4 bg-mainbg p-0 py-5">
         <section className="flex flex-col gap-3">
           <header className="flex items-center gap-3">
             <h1 className="flex items-center gap-3">
@@ -37,19 +37,16 @@ export default function ArticleCard({
               </Avatar>
               <span className="text-base font-extralight">{userName}</span>
             </h1>
-            <Separator
-              orientation="vertical"
-              className="h-4 bg-text-100 dark:bg-text-200"
-            />
+            <Separator orientation="vertical" className="h-4 bg-subgray" />
             <aside className="text-xs">{format(createdAt, "yyyy.LL.dd")}</aside>
           </header>
           <main className="w-full space-y-2">
             <h1 className="truncate text-lg">{title}</h1>
-            <h2 className="truncate text-xs text-text-200">{content}</h2>
+            <h2 className="truncate text-xs text-subgray">{content}</h2>
           </main>
         </section>
       </CardContent>
-      <CardFooter className="flex-row-reverse bg-bg-100 px-0 text-xs text-primary-100">
+      <CardFooter className="flex-row-reverse bg-mainbg px-0 text-xs text-active">
         {category}
       </CardFooter>
     </Card>
