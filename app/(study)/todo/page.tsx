2f0c70660da01d3d7025868818aa08232757fc7c
Ik <ci980704@naver.com>
2024-08-06 13:32:41 +0900

    feat: 라우팅 구조 변경 및 auth 정보를 통해 깃헙 프로필 사진 연동 구현 (#41)

---
+++ app/(study)/todo/page.tsx
@@ -0,0 +1,7 @@
+export default function Todo() {
+  return (
+    <div className="min-h-screen w-full max-w-[1440px] bg-bg-100 pt-16">
+      임시 Todo 페이지
+    </div>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import { auth } from "@/auth";
 import Logo from "@/public/logo.svg";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
 
 import {
   NavigationMenu,
@@ -19,7 +20,12 @@ const ROUTE_PAGE = [
   },
 ];
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+  const avatarImage = session?.user?.image
+    ? session.user.image
+    : "https://github.com/shadcn.png";
+
   return (
     <header className="fixed top-0 z-10 h-16 w-full border-b border-accent-200 bg-bg-100 px-[113px]">
       <div className="flex h-full w-full max-w-[1440px] items-center justify-between">
@@ -40,7 +46,7 @@ export default function Header() {
           </NavigationMenu>
         </div>
         <Avatar className="h-8 w-8">
-          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarImage src={avatarImage} />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </div>
