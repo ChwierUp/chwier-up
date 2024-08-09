6c38e3c3978ea29429e0775dc8475ba709c96356
SoJuSo <asdfg7123@gmail.com>
2024-08-10 02:45:20 +0900

    feat: article GET API에서 페이지네이션, 키워드 검색 지원 업데이트

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,15 +1,50 @@
 /* eslint-disable camelcase */
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-import { auth } from "@/auth";
+import prisma from "@/utils/supabase/prismaClient";
 
-const prisma = new PrismaClient();
-
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
-    const articles = await prisma.articles.findMany();
+    const searchParams = request.nextUrl.searchParams;
+    const page = parseInt(searchParams.get("page") || "1", 10);
+    const limit = parseInt(searchParams.get("limit") || "10", 10);
+    const keyword = searchParams.get("keyword");
 
-    return NextResponse.json(articles, { status: 200 });
+    const skip = (page - 1) * limit;
+
+    let whereClause = {};
+    if (keyword) {
+      whereClause = {
+        OR: [
+          { title: { contains: keyword, mode: "insensitive" } },
+          { content: { contains: keyword, mode: "insensitive" } },
+        ],
+      };
+    }
+
+    const [articles, totalCount] = await Promise.all([
+      prisma.articles.findMany({
+        where: whereClause,
+        skip, // skip 사용
+        take: limit, // take 사용
+        orderBy: { created_at: "asc" },
+      }),
+      prisma.articles.count({ where: whereClause }),
+    ]);
+
+    const totalPages = Math.ceil(totalCount / limit);
+
+    return NextResponse.json(
+      {
+        articles,
+        pagination: {
+          page,
+          limit,
+          totalCount,
+          totalPages,
+        },
+      },
+      { status: 200 },
+    );
   } catch (error) {
     console.error(error);
     if (error instanceof Error) {
@@ -19,72 +54,6 @@ export async function GET() {
         { error: "An unknown error occurred" },
         { status: 400 },
       );
-    }
-  }
-}
-
-export async function POST(req: NextRequest) {
-  try {
-    const session = await auth();
-    const userId = session?.user?.id;
-    if (!userId) {
-      return NextResponse.json(
-        { error: "User ID is required" },
-        { status: 400 },
-      );
-    }
-
-    const body = await req.json();
-
-    const data = await prisma.articles.create({
-      data: {
-        user_id: userId,
-        created_at: new Date(),
-        updated_at: new Date(),
-        title: body.ogTitle,
-        url: body.url,
-        content: body.ogDescription,
-        category: body.category,
-        img_url: body.ogImage,
-        user_name: session?.user?.name as string,
-        user_image: session?.user?.image as string,
-      },
-    });
-
-    return NextResponse.json(data, { status: 201 });
-  } catch (error) {
-    console.error("Error in POST handler:", error);
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 400 });
-    } else {
-      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
-    }
-  }
-}
-
-export async function DELETE(req: NextRequest) {
-  try {
-    const url = new URL(req.url);
-    const id = url.searchParams.get("id");
-
-    if (!id) {
-      return NextResponse.json({ error: "ID is required" }, { status: 400 });
-    }
-
-    await prisma.articles.delete({
-      where: { article_id: Number(id) },
-    });
-
-    return NextResponse.json(
-      { message: "Article deleted successfully" },
-      { status: 200 },
-    );
-  } catch (error) {
-    console.error("Error in DELETE handler:", error);
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 400 });
-    } else {
-      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
     }
   }
 }
