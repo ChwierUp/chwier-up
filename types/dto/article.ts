d4a3c713842c74869c387da870201405819ecb8c
SoJuSo <asdfg7123@gmail.com>
2024-08-10 02:44:51 +0900

    feat: article API 타입 업데이트

--- types/dto/article.ts
+++ types/dto/article.ts
@@ -1,3 +1,11 @@
+export type FetchArticlesParams = {
+  keyword: string | number | readonly string[] | undefined;
+  page?: number;
+  limit?: number;
+  totalCount?: number;
+  totalPages?: number;
+};
+
 export interface ArticleResponseType {
   article_id: string;
   user_id: string;
@@ -9,6 +17,11 @@ export interface ArticleResponseType {
   updated_at: string;
   content: string;
   category: string;
+}
+
+export interface ArticlePageResponseType {
+  articles: ArticleResponseType[]; // ArticleResponseType 배열
+  params: FetchArticlesParams; // FetchArticlesParams 타입
 }
 
 export interface ArticlePostRequestType {
