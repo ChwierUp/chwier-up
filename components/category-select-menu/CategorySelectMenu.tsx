fd2b92c86d933e3a5b35856cf521aa8c638ae71c
SoJuSo <asdfg7123@gmail.com>
2024-08-20 11:53:25 +0900

    feat: Category 메뉴 컴포넌트에서 state를 사용하지 않도록 수정

--- components/category-select-menu/CategorySelectMenu.tsx
+++ components/category-select-menu/CategorySelectMenu.tsx
@@ -1,32 +1,31 @@
-"use client";
-
-import { useCallback, useState } from "react";
 import { RadioGroup } from "../radio-group";
 import CategoryMenuItem from "./CategoryMenuItem";
 
-export default function CategorySelectMenu() {
-  const [category, setCategory] = useState("develop");
-  const handleValueChange = useCallback((value: string) => {
-    setCategory(value);
-  }, []);
-
+export default function CategorySelectMenu({
+  category,
+  handleValueChange,
+}: {
+  category: string;
+  handleValueChange: (value: string) => void;
+}) {
   return (
     <>
       <p className="mb-3 text-xl font-bold">✍🏻 카테고리</p>
       <RadioGroup
         onValueChange={handleValueChange}
-        defaultValue={category}
+        defaultValue={category === "" ? "all" : category}
         className="flex w-fit gap-6 rounded-md px-6 py-3 dark:bg-[#333340]"
       >
+        <CategoryMenuItem value="all" id="all" labelText="전체" />
         <CategoryMenuItem value="develop" id="develop" labelText="개발" />
         <CategoryMenuItem
-          value="selfDevelop"
-          id="selfDevelop"
+          value="self-development"
+          id="self-development"
           labelText="자기계발"
         />
         <CategoryMenuItem
-          value="interview"
-          id="interview"
+          value="recruitment"
+          id="recruitment"
           labelText="면접/이력서"
         />
         <CategoryMenuItem value="etc" id="etc" labelText="기타" />
